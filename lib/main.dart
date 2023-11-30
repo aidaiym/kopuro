ad26c7057184bbe06816e905fb3158b90f88803f
Aidaiym <omurbekovnaaidaiym@gmail.com>
2023-11-29 17:44:37 -0800

    update pod

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() async {
 
   await Firebase.initializeApp();
   final authenticationRepository = AuthenticationRepository();
-  await authenticationRepository.user.first;
+  // await authenticationRepository.user.first;
   // runApp(const MyApp());
   runApp(App(authenticationRepository: authenticationRepository));
 }
